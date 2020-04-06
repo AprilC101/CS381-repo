ac28f0b2caa27f82a297388e752a3763a992d384
paynem <49207892+paynem@users.noreply.github.com>
2020-04-05 17:41:26 -0700

    Added a, b, and c of part 1 to code
    I'll add more comments later.  I tested these, and they do seem to work.

--- cs381_hw1.hs
+++ cs381_hw1.hs
@@ -1,12 +1,39 @@
+import Data.List (nub,sort)
+norm :: Ord a => [a] -> [a]
+norm = sort . nub
 
 {-
 Homework 1
 
 Group Members: April Child, 
 
-Excersize 1 collaberators:  
+Exercise 1 collaberators:  
 
-Excersize 2 collaberators:  
-
-Excersize 3 collaberators: 
+Exercise 2 collaberators:  
+Exercise 3 collaberators: 
 -}
+
+-- Representations, I guess?
+type Bag a = [(a, Int)]
+
+eBag :: Bag a
+eBag = []
+
+-- (a) inserting element into a multiset
+ins :: Eq a => a -> Bag a -> Bag a
+ins x [] = [(x,1)]
+ins x ((y,z):xs)   | x == y    = (y, z+1):xs
+                | otherwise = (y,z):ins x xs
+
+-- (b) deleting element from a multiset
+del :: Eq a => a -> Bag a -> Bag a
+del x[] = []
+del x ((y,z):xs)    | x == y && z >= 2   = (y, z-1):xs
+                    | x == y && z == 1  = xs
+                    | otherwise = (y,z):del x xs
+
+-- (c) Converting list into a multiset
+bag :: Eq a => [a] -> Bag a
+bag bs = build bs eBag
+    where   build [] nb = nb
+            build (x:xs) nb = build xs (ins x nb)
