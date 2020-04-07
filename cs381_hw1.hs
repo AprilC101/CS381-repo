cfdf0e76bd76638dd4875171f3bf867a15deb95c
paynem <49207892+paynem@users.noreply.github.com>
2020-04-07 14:22:15 -0700

    Complete a-c of exercise 2
    Still working on part d of exercise 2

--- cs381_hw1.hs
+++ cs381_hw1.hs
@@ -84,3 +84,41 @@ isbag b1 b2 = inter b1 b2 eBag
 -- This one was so easy that I'm worried that I interpreted the instructions incorrectly.
 size :: Bag a -> Int
 size xs =  sum [y | (x,y) <- xs]  
+
+
+-- Exercise 2
+type Node = Int
+type Edge = (Node,Node)
+type Graph = [Edge]
+type Path = [Node]
+
+g :: Graph
+g = [(1,2),(1,3),(2,3),(2,4),(3,4)]
+h :: Graph
+h = [(1,2),(1,3),(2,1),(3,2),(4,4)]
+
+--(a) Define the function nodes :: Graph -> [Node] that computes the list of nodes contained in a given graph.
+--For example, nodes g = [1,2,3,4] 
+nodes :: Graph -> [Node]
+nodes [] = []
+nodes ((x,y):xs) = norm (x:y:nodes xs)
+
+
+--(b) Define the function suc :: Node -> Graph -> [Node] that computes the list of successors for a node in a
+--given graph. For example, suc 2 g = [3,4] , suc 4 g = [] , and suc 4 h = [4]
+suc :: Node -> Graph -> [Node]
+suc n [] = []
+suc n ((x,y):xs)        | n == x        = norm(y:suc n xs)
+                        | otherwise     = suc n xs
+
+
+--(c) Define the function detach :: Node -> Graph -> Graph that removes a node together with all of its incident
+--edges from a graph. For example, detach 3 g = [(1,2),(2,4)] and detach 2 h = [(1,3),(4,4)]
+detach :: Node -> Graph -> Graph
+detach n [] = []
+detach n ((x,y):xs)     | n == x || n == y      = detach n xs
+                        | otherwise             = norm((x,y) : detach n xs)
+                        
+                        
+--(d) Define the function cyc :: Int -> Graph that creates a cycle of any given number. For example, cyc 4 =
+--[(1,2),(2,3),(3,4),(4,1)]                         
